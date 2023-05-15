0e7092ddb8619156951c8be1b54eaf48ae8622a9
ajita0303 <ajita.agrawal1107@gmail.com>
2023-05-15 23:09:30 +0530

    initial

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "e4cd847caa5f495ada1dd9df2fd2fd69",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
